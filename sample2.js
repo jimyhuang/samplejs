6bdb6cc432f3747352a49897aafe4d6a346bffab
Jimmy Huang <jimmy@netivism.com.tw>
2014-12-08 12:17:28 +0800

    Add samples.

---
+++ sample2.js
@@ -0,0 +1,7 @@
+var i = 10;
+setInterval(function(j){
+  j++;
+  console.log(j);
+}, 1000, i);
+
+// 這段程式會印出？
---
+++ sample4.js
@@ -0,0 +1,25 @@
+// 這一段貼到 chrome console，會印出？
+var location = function(){
+  console.log('cute yana');
+}
+
+console.log(location);
+
+
+
+
+
+
+
+
+
+/*
+(function(){
+  var location = function(){
+    console.log('cute yana');
+  };
+
+  console.log(location);
+  console.log(location());
+})();
+// console.log(location);
